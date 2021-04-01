08c41f8afb6be7a6aa144e7cb19d269fe4d17d2f
Fran Anusic <fran.anusic@hotmail.com>
2021-04-01 15:17:40 +0200

    Add project files.

---
+++ Zadatak1/Controllers/QueryPodaciController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zadatak1.Controllers
+{
+    public class QueryPodaciController : Controller
+    {
+        public ViewResult QueryPodaci() 
+        {
+            return View("Osoba");
+        }
+        [HttpPost]
+        public ViewResult QueryPodaci(string ime_prezime)
+        {
+            string pozdrav = "Dobar dan, " + ime_prezime + "!";
+            return View("Osoba", (object)pozdrav);
+        }
+    }
+}
